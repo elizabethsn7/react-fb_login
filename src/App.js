07c5461e91de0935b3a254b1c380f63a05a2ab6d
Elizabeth Kipp <elizabethkipp@MacBook-Pro-4.local>
2021-11-10 21:00:54 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  const [login, setLogin] = useState(false); // set up login
+  const [data, setData] = useState({}); //set up FB data
+  const [picture, setPicture] = useState(''); // set up fb profile image
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+  return (
+    <div class="container">
+      <Card>
+        <Card.Header>
+          <h1>My React App</h1>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            {!login && (
+              <>
+                <h3>Please login using one of the following</h3>
+                {/*  Login Form */}
+                <LoginForm />
+                {/*  FB Login Button */}
+                <FacebookLogin
+                  appId="586658149268849"
+                  autoLoad={false}
+                  fields="name, email, picture"
+                  scope="public_profile, user_friends"
+                  callback={responseFacebook}
+                  icon="fa-facebook"
+                />
+              </>
+            )}
+            {login && <Home fbpic={picture} fbdata={data} />}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+function LoginForm() {
+  return (
+    <form className="border mt-3 mb-5 p-3 bg-white">
+      <label className="m-2">Name:</label>
+      <input type="text" name="name" placeholder="Your name" />
+      <label className="m-2">Email:</label>
+      <input type="email" name="email" placeholder="Your email" />
+      <input
+        type="submit"
+        value="login"
+        className="btn bg-success text-white my-3"
+      />
+    </form>
+  );
+}
+
+function Home({ fbpic, fbdata }) {
+  return (
+    <React.Fragment>
+      <img src={fbpic} alt={fbdata.name} />
+      <h3 className="d-inline text-success mx-2">Welcome Back {fbdata.name}</h3>
+      <p className="my-5">This is the home page of the app</p>
+    </React.Fragment>
+  );
+}
+export default App;
